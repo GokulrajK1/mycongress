e2d310c73650e5fc04cbcf4fc7403a240af64092
mridhanbalaji <mridhanbalaji210706@gmail.com>
2023-09-02 22:24:27 -0400

    routing

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 import db from "./firebase"
 import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./Homepage";
+import Login from "./pages/Login";
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+          <Route index element={<Homepage />} />
+          <Route path="login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Login.js
@@ -0,0 +1,12 @@
+import './Login.css';
+
+function Login() {
+    return (
+      <div className="App">
+        <h1>LOGIN</h1>
+      </div>
+    );
+  }
+  
+  export default Login;
+  //Testing if working
