a5be1fff6cbeb7e4adbf46d987d7319fdb083d7e
mridhanbalaji <mridhanbalaji210706@gmail.com>
2023-09-02 22:46:20 -0400

    Login Form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,20 @@
 import './Login.css';
 
+
+const loginHandle =(e) => {
+    console.log("Hello")
+}
+
 function Login() {
     return (
       <div className="App">
         <h1>LOGIN</h1>
+
+        <form onSubmit={(e) => loginHandle(e)}>
+            <input name='email' placeholder='Email'></input> <br/>
+            <input name='password' type='password' placeholder='password'></input><br/>
+            <button>SignIn</button>
+        </form>
       </div>
     );
   }
